c0f458b518d8de5bcadec33463cf301d12ea69b5
Gowtham <gowthamkumarc9@gmail.com>
2025-04-10 23:33:54 +0530

    Updated backend URL for production

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { UserContextProvider } from './UserContext';
 import Routes from './Routes';
 
 function App() {
-  axios.defaults.baseURL="http://localhost:4040";
+  axios.defaults.baseURL="http://localhost:https://ultra-chat-app.onrender.com";
   axios.defaults.withCredentials=true;
   return (
     <UserContextProvider>
